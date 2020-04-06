e50b17896846e47ffaa263c713683e7bf7009198
st0yk0 <stoyko_mashev@abv.bg>
2020-04-06 09:43:31 +0300

    initial commit

---
+++ src/main/java/com/pragmatic/uploadingfiles/UploadingFilesApplication.java
@@ -0,0 +1,26 @@
+package com.pragmatic.uploadingfiles;
+
+import com.pragmatic.uploadingfiles.storage.StorageProperties;
+import com.pragmatic.uploadingfiles.storage.StorageService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
+public class UploadingFilesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UploadingFilesApplication.class);
+    }
+
+    @Bean
+    CommandLineRunner init(StorageService storageService) {
+        return (args) -> {
+            storageService.deleteAll();
+            storageService.init();
+        };
+    }
+}
